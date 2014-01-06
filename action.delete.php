3d030c38e7f09c289a98c6dd91e9c2cc7d119d3b
besstiolle <contact@furie.be>
2014-01-06 01:09:46 +0100

    add Delete action

--- action.delete.php
+++ action.delete.php
@@ -1,4 +1,19 @@
 <?php
 
+//Default values
+$titleParam = null;
+$langParam = null;
+$smarty = cmsms()->GetSmarty();
+
+/*******************************************/
+
+//Common initialization
+include_once('inc.initialization.php');
+
+//Update to "old version"
+$query = "UPDATE {$version->getDbname()} SET status={$version::$STATUS_OLD} WHERE status={$version::$STATUS_CURRENT} AND lang_id={$lang->get($lang->getPk()->getName())} AND page_id={$page->get($page->getPk()->getName())}";
+OrmDb::execute($query);
+
+$this->RedirectForFrontEnd($id, $returnid, 'default', $params);
 
 ?>
