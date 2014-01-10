2a1afa6881ae581b6f1b66e000ab35c03ad402ec
besstiolle <contact@furie.be>
2014-01-10 13:37:55 +0100

    improve errors on edit&view action

--- action.default.php
+++ action.default.php
@@ -33,34 +33,36 @@ if(count($langs) == 0){
 	$lang = $langs[0];
 }
 
-if($lang != null){
-	//Get Version
-	$example = new OrmExample();
-	$example->addCriteria('title', OrmTypeCriteria::$EQ, array($titleParam));
-	$example->addCriteria('lang_id', OrmTypeCriteria::$EQ, array($lang->get($lang->getPk()->getName())));
-	
-	if($version_id != null){ // Case wiki/en_US/home/view/2
-		$example->addCriteria('version_id', OrmTypeCriteria::$EQ, array($version_id));
-	} else {
-		$example->addCriteria('status', OrmTypeCriteria::$EQ, array(Version::$STATUS_CURRENT));
-	}
-
-	$versions = OrmCore::findByExample(new Version(),$example, null, new OrmLimit(0,1));
-	if(count($versions) == 0){
-		$version = null;
-		$vals = null;
-		$page = null;
-	} else {
-		$version = $versions[0];
-		$vals = $version->getValues();
-		$page = OrmCore::findById(new Page(),$version->get('page_id'));
-	}
-		
-} else { //Error cases
-	echo "The lang {$langParam} is not an known lang.";
+if($lang == null){
+	$errors[] = 'lang_mandatory';
+	$url = $this->CreateLink ($id, "default", $returnid, '', array(), '', true, false, '', '', RouteMaker::getViewRoute('en_US', 'home'));
+	$smarty->assign('errors',$errors);
+	$smarty->assign('url',$url);
+	echo $this->ProcessTemplate('message.tpl');
 	return;
 }
+//Get Version
+$example = new OrmExample();
+$example->addCriteria('title', OrmTypeCriteria::$EQ, array($titleParam));
+$example->addCriteria('lang_id', OrmTypeCriteria::$EQ, array($lang->get($lang->getPk()->getName())));
 
+if($version_id != null){ // Case wiki/en_US/home/view/2
+	$example->addCriteria('version_id', OrmTypeCriteria::$EQ, array($version_id));
+} else {
+	$example->addCriteria('status', OrmTypeCriteria::$EQ, array(Version::$STATUS_CURRENT));
+}
+
+$versions = OrmCore::findByExample(new Version(),$example, null, new OrmLimit(0,1));
+if(count($versions) == 0){
+	$version = null;
+	$vals = null;
+	$page = null;
+} else {
+	$version = $versions[0];
+	$vals = $version->getValues();
+	$page = OrmCore::findById(new Page(),$version->get('page_id'));
+}
+	
 
 if(!empty($params['werrors'])){
 	$errors = explode('|',$params['werrors']);
